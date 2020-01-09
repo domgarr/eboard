bd8a1fac6ce5557e1ff6c2af00638e2a2a8f8f80
unknown <garreffd@uwindsor.ca>
2020-01-08 21:16:16 -0500

    Add increment/decrease/reset of score and swap scores sides

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,5 +1,22 @@
-import { TestBed, async } from '@angular/core/testing';
+import { TestBed, async, ComponentFixture } from '@angular/core/testing';
 import { AppComponent } from './app.component';
+
+let component : AppComponent;
+let fixture : ComponentFixture<AppComponent>;
+let compiled : any;
+let app : any;
+
+let redScoreText : HTMLElement;
+let blueScoreText : HTMLElement;
+
+let redScoreIncButton : HTMLElement;
+let redScoreDecButton : HTMLElement;
+let blueScoreIncButton : HTMLElement;
+let blueScoreDecButton : HTMLElement;
+
+let swappedButton : HTMLElement;
+let resetButton : HTMLElement;
+
 
 describe('AppComponent', () => {
   beforeEach(async(() => {
@@ -8,6 +25,24 @@ describe('AppComponent', () => {
         AppComponent
       ],
     }).compileComponents();
+
+    fixture = TestBed.createComponent(AppComponent);
+    compiled = fixture.debugElement.nativeElement;
+    app = fixture.debugElement.componentInstance;
+
+    fixture.detectChanges();
+
+    redScoreText = fixture.nativeElement.querySelector("#redScoreText");
+    blueScoreText = fixture.nativeElement.querySelector("#blueScoreText");
+
+    redScoreIncButton = fixture.nativeElement.querySelector("#redScoreIncButton");
+    redScoreDecButton = fixture.nativeElement.querySelector("#redScoreDecButton");
+
+    blueScoreIncButton = fixture.nativeElement.querySelector("#blueScoreIncButton");
+    blueScoreDecButton = fixture.nativeElement.querySelector("#blueScoreDecButton");
+
+    swappedButton = fixture.nativeElement.querySelector('#swappedButton');
+    resetButton = fixture.nativeElement.querySelector('#resetButton');
   }));
 
   it('should create the app', () => {
@@ -16,16 +51,156 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'eBoard'`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('eBoard');
+  it('On initialization RedScoreText should equal 00', ()=>{
+    expect(redScoreText.textContent).toBe("00");
   });
 
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('eBoard app is running!');
+  it('On initialization BlueScoreText should equal 00', ()=>{
+    expect(blueScoreText.textContent).toBe("00");
   });
+
+  it('function prependZero(1) should return a string value of "01" ', ()=>{
+    let testString = app.prependZeroAndConvertToString(1);
+    expect(testString).toBe("01");
+  });
+
+  it('function prependZero(10) should return a string value of "10" ', ()=>{
+    let testString = app.prependZeroAndConvertToString(10);
+    expect(testString).toBe("10");
+  });
+
+  it('function incrementScore(0) should return 1', ()=>{
+    let mutatedScore = app.incrementScore(0);
+    expect(mutatedScore).toBe(1);
+  });
+
+  it('function decrementScore(0) should return 0', ()=>{
+    let mutatedScore = app.decrementScore(0);
+    expect(mutatedScore).toBe(0);
+  });
+
+  it('function decrementScore(9) should return 8', ()=>{
+    let mutatedScore = app.decrementScore(9);
+    expect(mutatedScore).toBe(8);
+  });
+
+  it('Clicking the BlueScoreIncrement Button will increase the score from 00 to 01', ()=>{
+    blueScoreIncButton.click();
+
+    expect(app.blueScore).toBe(1);
+    expect(app.blueScoreText).toBe("01");
+  });
+
+  it('Clicking the BlueScoreIncrement Button 10 times will increase the score from 00 to 10', ()=>{
+    for(var i = 0; i < 10 ; i++){
+      blueScoreIncButton.click();
+    }
+
+    expect(app.blueScore).toBe(10);
+    expect(app.blueScoreText).toBe("10");
+  });
+
+  it('Clicking the RedScoreIncrement Button will increase the score from 00 to 01', ()=>{
+    redScoreIncButton.click();
+
+    expect(app.redScore).toBe(1);
+    expect(app.redScoreText).toBe("01");
+  });
+
+  it('Clicking the RedScoreIncrement Button 10 times will increase the score from 00 to 10', ()=>{
+    for(var i = 0; i < 10 ; i++){
+      redScoreIncButton.click();
+    }
+
+    expect(app.redScore).toBe(10);
+    expect(app.redScoreText).toBe("10");
+  });
+
+  it('Clicking the RedScoreDecrement Button will decrease the score from 00 to 00', ()=>{
+    redScoreDecButton.click();
+
+    expect(app.redScore).toBe(0);
+    expect(app.redScoreText).toBe("00");
+  });
+
+  it('Clicking the RedScoreDecrement Button will decrease the score from 01 to 00', ()=>{
+    redScoreIncButton.click();
+
+    expect(app.redScore).toBe(1);
+    expect(app.redScoreText).toBe("01");
+
+    redScoreDecButton.click();
+
+    expect(app.redScore).toBe(0);
+    expect(app.redScoreText).toBe("00");
+  });
+
+  it('Clicking the RedScoreDecrement Button will decrease the score from 10 to 09', ()=>{
+    for(var i = 0; i < 10 ; i++){
+      redScoreIncButton.click();
+    }
+
+    expect(app.redScore).toBe(10);
+    expect(app.redScoreText).toBe("10");
+
+    redScoreDecButton.click();
+
+    expect(app.redScore).toBe(9);
+    expect(app.redScoreText).toBe("09");
+  });
+
+  it('Clicking the BlueScoreDecrement Button will decrease the score from 00 to 00', ()=>{
+    blueScoreDecButton.click();
+
+    expect(app.blueScore).toBe(0);
+    expect(app.blueScoreText).toBe("00");
+  });
+
+  it('Clicking the BlueScoreDecrement Button will decrease the score from 01 to 00', ()=>{
+    blueScoreIncButton.click();
+
+    expect(app.blueScore).toBe(1);
+    expect(app.blueScoreText).toBe("01");
+
+    blueScoreDecButton.click();
+
+    expect(app.blueScore).toBe(0);
+    expect(app.blueScoreText).toBe("00");
+  });
+
+  it('Clicking the BlueScoreDecrement Button will decrease the score from 10 to 09', ()=>{
+    for(var i = 0; i < 10 ; i++){
+      blueScoreIncButton.click();
+    }
+
+    expect(app.blueScore).toBe(10);
+    expect(app.blueScoreText).toBe("10");
+
+    blueScoreDecButton.click();
+
+    expect(app.blueScore).toBe(9);
+    expect(app.blueScoreText).toBe("09");
+  });
+
+  it('Clicking swap will set the swapped from false to true', ()=>{
+    swappedButton.click();
+    expect(app.swapped).toBe(true);
+  });
+
+  it('Clicking swap twice will set the swapped from false to false', ()=>{
+    swappedButton.click();
+    swappedButton.click();
+    expect(app.swapped).toBe(false);
+  });
+
+  it('Reset the scores back to zero. RedScore/Text should be 0/"00" and BlueScore/Text should be 0/"00"', ()=>{
+    resetButton.click();
+
+    expect(app.blueScore).toBe(0);
+    expect(app.blueScoreText).toBe("00");
+    expect(app.redScore).toBe(0);
+    expect(app.redScoreText).toBe("00");
+  });
+
+
 });
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,11 +10,70 @@ import { Component, ViewChild, ElementRef, OnInit, AfterViewInit } from '@angula
 export class AppComponent implements OnInit {
   @ViewChild("bteConnectButton", {static:false}) bteButton : ElementRef;
 
+  private swapped : boolean = false;
+
+  private redScoreText : string = "00";
+  private blueScoreText : string = "00";
+
+  private redScore : number = 0;
+  private blueScore : number = 0;
+
   constructor(){
   }
 
   ngOnInit(){
 
+  }
+
+  incrementScore(value : number) : number{
+    return value + 1;
+  }
+
+  decrementScore(value : number) : number{
+    if(value <= 0){
+      return 0;
+    }
+    return value - 1;
+  }
+
+  /* If the score is less than 10 prepend a "0" for design purposes"
+  takes a number and returns a string. */
+  prependZeroAndConvertToString(score : number) : string {
+    if(score <= 9){
+      return "0" + score;
+    }
+    return score + "";
+  }
+
+  blueScoreInc(){
+    this.blueScore = this.incrementScore(this.blueScore);
+    this.blueScoreText = this.prependZeroAndConvertToString(this.blueScore);
+  }
+
+  redScoreInc(){
+    this.redScore = this.incrementScore(this.redScore);
+    this.redScoreText = this.prependZeroAndConvertToString(this.redScore);
+  }
+
+  blueScoreDec(){
+    this.blueScore = this.decrementScore(this.blueScore);
+    this.blueScoreText = this.prependZeroAndConvertToString(this.blueScore);
+  }
+
+  redScoreDec(){
+    this.redScore = this.decrementScore(this.redScore);
+    this.redScoreText = this.prependZeroAndConvertToString(this.redScore);
+  }
+
+  swap(){
+    this.swapped = !this.swapped;
+  }
+
+  resetScore(){
+    this.redScore = 0;
+    this.blueScore = 0;
+    this.redScoreText = "00";
+    this.blueScoreText = "00";
   }
 
 
